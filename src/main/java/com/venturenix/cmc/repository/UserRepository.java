02cb51eaae23084cc5b1b378a39e46f7d381d4f1
psivincentchan <cheongho.vincent@gmail.com>
2024-02-18 17:51:27 +0800

    first commit
    first commit

---
+++ src/main/java/com/venturenix/cmc/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.venturenix.cmc.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.venturenix.cmc.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
+}
